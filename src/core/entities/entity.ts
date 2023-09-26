0d010425ceae0cb47238279145d03ee476952d84
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2023-09-26 10:12:46 -0300

    Abstracting entity creation

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,16 +1,15 @@
-import { randomUUID } from "node:crypto"
 import { UniqueEntityId } from "./unique-entity-id"
 
 export class Entity<Props> {
   private _id: UniqueEntityId
-  protected props: any
+  protected props: Props
 
   get id() {
     return this._id
   }
 
-  constructor(props: any, id?: string) {
+  protected constructor(props: Props, id?: UniqueEntityId) {
     this.props = props
-    this._id = new UniqueEntityId(id)
+    this._id = id ?? new UniqueEntityId()
   }
 } 
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,6 +1,7 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityId } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 
 interface QuestionProps {
   authorId: UniqueEntityId
@@ -8,10 +9,20 @@ interface QuestionProps {
   title: string
   content: string
   slug: Slug
-  createAt: Date
-  updateAt?: Date
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {
+  static create(
+    props: Optional<QuestionProps, 'createdAt'>, 
+    id?: UniqueEntityId
+  ) {
+    const question = new Question({
+      ...props,
+      createdAt: new Date(),
+    }, id)
 
+    return question
+  }
 }
