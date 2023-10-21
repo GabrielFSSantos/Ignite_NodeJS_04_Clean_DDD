884f6ca368943659bcc0bc2109326f7796d9dd01
Gabriel Santos <59262903+GabrielFSSantos@users.noreply.github.com>
2023-10-21 11:26:47 -0300

    Use case: Delete answer

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -2,9 +2,25 @@ import { AnswersRepository } from '@/domain/forum/application/repositories/answe
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
 export class InMemoryAnswersRepository implements AnswersRepository {
-  public itens: Answer[] = []
+  public items: Answer[] = []
+
+  async findById(id: string): Promise<Answer | null> {
+    const answer = this.items.find((item) => item.id.toString() === id)
+
+    if (!answer) {
+      return null
+    }
+
+    return answer
+  }
 
   async create(answer: Answer): Promise<void> {
-    this.itens.push(answer)
+    this.items.push(answer)
+  }
+
+  async delete(answer: Answer): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items.splice(itemIndex, 1)
   }
 }
