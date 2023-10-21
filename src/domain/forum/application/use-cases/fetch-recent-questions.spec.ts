6e40c3b9566c7bc2628cf2d95214fcc6fb8b473d
Gabriel Santos <59262903+GabrielFSSantos@users.noreply.github.com>
2023-10-21 14:36:24 -0300

    Use case: Fetch question answers

--- src/domain/forum/application/use-cases/fetch-recent-questions.spec.ts
+++ src/domain/forum/application/use-cases/fetch-recent-questions.spec.ts
@@ -1,7 +1,6 @@
 import { FetchRecentQuestionsUseCase } from './fetch-recent-questions'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { makeQuestion } from 'test/factories/make-question'
-import { Slug } from '../../enterprise/entities/value-objects/slug'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: FetchRecentQuestionsUseCase
