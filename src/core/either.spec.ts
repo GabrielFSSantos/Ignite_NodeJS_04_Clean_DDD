82e3ec61e20d5b4dfc0ba51fdf2db82bc6083ac6
Gabriel Santos <59262903+GabrielFSSantos@users.noreply.github.com>
2023-10-28 12:26:32 -0300

    Creating generic errors

--- src/core/either.spec.ts
+++ src/core/either.spec.ts
@@ -1,18 +1,14 @@
 import { Either, left, right } from './either'
 
-function doSomething(result: boolean): Either<string, number> {
+function doSomething(result: boolean): Either<string, string> {
   if (result) {
-    return right(10)
+    return right('success')
   }
   return left('error')
 }
 
 test('success result', () => {
   const result = doSomething(true)
-
-  if (result.isRight()) {
-    console.log(result.value)
-  }
 
   expect(result.isRight()).toBe(true)
   expect(result.isLeft()).toBe(false)
